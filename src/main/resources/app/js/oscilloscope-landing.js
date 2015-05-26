fc83163aafe8b38a87c8a272f7d2966d791be010
Toby Lawrence <tobias.lawrence@gmail.com>
2015-05-26 14:14:40 -0400

    Lots of UI revamping.
    This is half creating a new styling for the landing and monitoring pages, and half making the existing Hystrix styling -- namely the circuit breaker views -- work correctly, and display correctly, with some of the defaults used by Foundation.

--- src/main/resources/app/js/oscilloscope-landing.js
+++ src/main/resources/app/js/oscilloscope-landing.js
@@ -22,16 +22,36 @@ $(document).ready(function() {
             alert.hide().insertAfter("#cluster").fadeIn(1000)
         })
 
+    // Track changes to our custom target textbox.
     $("#stream").on("input propertychange paste", function() {
         targetStateChanged()
     })
 
+    // Allow users to hit Enter when typing or pasting in a custom stream, rather than make
+    // them have to click the 'Monitor Cluster / Target' button.
+    $("#stream").on("keypress", function(e) {
+        var stream = $("#stream").val()
+        if(stream) {
+            if(e.which == 13) {
+                redirectToStream(stream)
+            }
+        }
+    })
+
+    // Set up our button to take us to the appropriate spot.
     $("#monitor").on("click", function(e) {
         e.stopPropagation()
         redirectToMonitor()
     })
 })
 
+/*
+    Tracks state changes to the target selectors.
+
+    Since we want to make sure you can't click on the 'Monitor Cluster / Target' button
+    if you have no option selected or specified, this keeps track of whether or not we
+    have a valid option to use, and disables/enables the button appropriately.
+ */
 function targetStateChanged() {
     var cluster = $("#cluster").val()
     var stream = $("#stream").val()
@@ -43,6 +63,13 @@ function targetStateChanged() {
     }
 }
 
+/*
+    Redirects the user to the monitor page.
+
+    Custom stream targets take precedence over a cluster selection here, since you may have
+    chosen something unwittingly, then just wanted to go to a custom location, and now you'd
+    otherwise be locked in to your cluster choice, since you can't deselect.
+ */
 function redirectToMonitor() {
     var cluster = $("#cluster").val()
     var stream = $("#stream").val()
@@ -54,10 +81,16 @@ function redirectToMonitor() {
     }
 }
 
+/*
+    Redirects the user to the monitor page in custom stream target mode.
+ */
 function redirectToStream(stream) {
-    console.log("redirect to stream: " + stream)
+    location.href="/monitor.html?stream=" + encodeURIComponent(stream)
 }
 
+/*
+    Redirects the user to the monitor page in cluster target mode.
+ */
 function redirectToCluster(cluster) {
-    console.log("redirect to cluster: " + cluster)
+    location.href="/monitor.html?cluster=" + encodeURIComponent(cluster)
 }
---
+++ src/main/resources/app/js/oscilloscope-monitor.js
@@ -0,0 +1,87 @@
+//Read a page's GET URL variables and return them as an associative array.
+// from: http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
+function getUrlVars()
+{
+    var vars = []
+    var hash = []
+
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=')
+        vars.push(hash[0])
+        vars[hash[0]] = hash[1]
+    }
+
+    return vars
+}
+
+// Create our monitors.  We leave them out here because we need to access them from the monitor
+// page to sort differently, etc.
+var hystrixMonitor = new HystrixCommandMonitor('dependencies', {includeDetailIcon: false})
+var dependencyThreadPoolMonitor = new HystrixThreadPoolMonitor('dependencyThreadPools')
+
+$(document).ready(function() {
+    // Get our stream/cluster values, and build our command/pool stream sources based on which one we have.
+    var stream = getUrlVars()["stream"]
+    var cluster = getUrlVars()["cluster"]
+
+    var eventStream = ""
+
+    if (stream != undefined) {
+        eventStream = "/proxy.stream?origin=" + stream
+
+        $("#monitor_type").html("Stream: <small>" + stream + "</small>")
+    } else if (cluster != undefined) {
+        eventStream = "/turbine.stream?cluster=" + cluster
+
+        $("#monitor_type").html("Cluster: <small>" + cluster + "</small>")
+    }
+
+    // Now load both our command and pool monitors asynchronously to avoid infinite spinner situations.
+    $(window).load(function () {
+        setTimeout(function () {
+            if (eventStream == "") {
+                $("#dependencies .loading").html("The 'stream' or 'cluster' argument was not provided.")
+                $("#dependencies .loading").addClass("failed")
+                $("#dependencyThreadPools .loading").html("The 'stream' or 'cluster' argument was not provided.")
+                $("#dependencyThreadPools .loading").addClass("failed")
+            } else {
+                // Sort by error+volume by default.
+                hystrixMonitor.sortByErrorThenVolume()
+
+                // Sort by volume by default.
+                dependencyThreadPoolMonitor.sortByVolume()
+
+                // Create our EventSource object which will immediately start streaming data from the server.
+                var source = new EventSource(eventStream)
+
+                // Now add our event listeners to actually process events and handle any errors.
+                source.addEventListener('message', hystrixMonitor.eventSourceMessageListener, false)
+                source.addEventListener('message', dependencyThreadPoolMonitor.eventSourceMessageListener, false)
+                source.addEventListener('error', function (e) {
+                    $("#dependencies .loading").html("Failed to connect to the command stream.")
+                    $("#dependencies .loading").addClass("failed");
+                    $("#dependencyThreadPools .loading").html("Failed to connect to the pool stream.")
+                    $("#dependencyThreadPools .loading").addClass("failed")
+
+                    if (e.eventPhase == EventSource.CLOSED) {
+                        // Connection was closed.
+                        console.log("Connection was closed on error: " + e)
+                    } else {
+                        console.log("Error occurred while streaming: " + e)
+                    }
+                }, false)
+            }
+        }, 0)
+    })
+})
+
+$(document).ready(function() {
+    $(".sub-nav dd").on("click", function() {
+        if(!$(this).hasClass("active")) {
+            $(this).parent().children("dd.active").removeClass("active")
+            $(this).addClass("active")
+        }
+    })
+})
