cbcd3f6d40780868b8f320a93aa17248543c6a4f
Toby Lawrence <tobias.lawrence@gmail.com>
2015-06-01 11:12:59 -0400

    Woops, missed the cluster stuff. :)

---
+++ src/main/java/com/bluestatedigital/oscilloscope/turbine/ConsulClusterDiscovery.java
@@ -0,0 +1,28 @@
+package com.bluestatedigital.oscilloscope.turbine;
+
+import com.ecwid.consul.v1.ConsistencyMode;
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.QueryParams;
+import com.ecwid.consul.v1.Response;
+
+import java.util.*;
+
+public class ConsulClusterDiscovery implements ClusterDiscovery {
+    @Override
+    public Collection<String> getClusters() {
+        HashSet<String> clusters = new HashSet<>();
+
+        try {
+            ConsulClient client = new ConsulClient();
+            Response<Map<String, List<String>>> response = client.getCatalogServices(new QueryParams(ConsistencyMode.DEFAULT));
+            for(Map.Entry<String, List<String>> entry : response.getValue().entrySet()) {
+                if(entry.getValue().contains("hystrix")) {
+                    clusters.add(entry.getKey());
+                }
+            }
+        } catch(Exception e) {
+        }
+
+        return clusters;
+    }
+}
